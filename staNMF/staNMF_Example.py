12d39bd4d3f980e86da34aac234b2305ed76c3aa
amyecampbell <amyecampbell@gmail.com>
2016-08-26 16:57:50 -0400

    removing messy print statements

--- staNMF/staNMF_Example.py
+++ staNMF/staNMF_Example.py
@@ -4,20 +4,18 @@ Example script using staNMF class. Reproduces Wu et al. 2016
 staNMF for drosophila spatial expression data by
 running staNMF for # principal patterns K between 15 and 30
 '''
-print("1")
+
 import staNMF as st
-print("2")
+
 # Construct NMF object with custom output folder name
 # Note: filename = 'example' uses the included
 # 'updatedexpressionpatterns.csv'  and parameter 'folderID'
 # is not required
 exampleNMF = st.staNMF(filename='example',
                        folderID="DrosophilaExpression")
-print("3")
 # Run NMF and output factorization solutions over the range
 # of K's
 exampleNMF.runNMF()
-print("4")
 # Calculate instability for each K
 exampleNMF.instability(k1=15, k2=30)
 

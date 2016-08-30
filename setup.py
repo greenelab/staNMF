84a69559280b58c978fb4c4c8913ec87590ca662
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2016-08-30 15:08:52 -0500

    Add __init__ and minor setup.py fixes (#1)

--- setup.py
+++ setup.py
@@ -9,23 +9,23 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-      name='staNMF',
-      version='0.1',
-      packages=find_packages(exclude=['spams']),
-      include_package_data=True,
-      license='LICENSE.txt',
-      description='python 2.7 implementation of stability NMF (Siqi Wu 2016)',
-      long_description=long_description,
-      url='https://github.com/greenelab/staNMF',
-      author='Greene Lab',
-      author_email='team@greenelab.com',
-      install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'spams'],
-      classifiers=[
+    name='staNMF',
+    version='0.1',
+    packages=['staNMF'],
+    include_package_data=True,
+    license='LICENSE.txt',
+    description='python 2.7 implementation of stability NMF (Siqi Wu 2016)',
+    long_description=long_description,
+    url='https://github.com/greenelab/staNMF',
+    author='Greene Lab',
+    author_email='team@greenelab.com',
+    classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
-                  ],
-      )
+    ],
+    install_requires=['numpy', 'pandas', 'scipy', 'matplotlib'],
+)
